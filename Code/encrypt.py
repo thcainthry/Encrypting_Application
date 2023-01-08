11012bb5c42d13668754aa56a89949638980a27c
Gentiana Idrizi <93874728+Gentiana-Idrizi@users.noreply.github.com>
2023-01-08 14:36:09 +0100

    Update encrypt.py

--- Code/encrypt.py
+++ Code/encrypt.py
@@ -3,3 +3,5 @@ import pyAesCrypt
 
 # Get the current working directory (the directory where the script is located)
 directory = os.getcwd()
+
+# Set the password that will be used to encrypt and decrypt the files
